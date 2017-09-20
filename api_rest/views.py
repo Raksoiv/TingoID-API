e2900d995464c455674e250b7ce8eeef16a7b52d
Mitkha <constanza.mitkha@gmail.com>
2017-09-20 19:34:06 -0300

    agregando login + handshaking

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.middleware.csrf import get_token
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+
+import json
 
 # Create your views here.
+
+def handshaking(request):
+	response_data = {
+		"csrf_token": get_token(request)
+	}
+
+	return HttpResponse(json.dumps(response_data), content_type = "application/json")
+
+def login(request):
+	if request.method == 'POST':
+		received_data = json.loads(request.body.decode('utf-8'))
+		user = authenticate(username=received_data['user'], password = received_data['pass'])
+		if user is not None:
+			return HttpResponse(json.dumps(True), content_type = "application/json")
+		else:
+			return HttpResponse(json.dumps(False), content_type = "application/json")
+
+
--- tingoid/settings.py
+++ tingoid/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'api_rest.Usuario' 
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
