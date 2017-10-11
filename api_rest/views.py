d421e371183126563f097a041164bdab82ac5e12
Mitkha <constanza.mitkha@gmail.com>
2017-10-11 17:03:13 -0300

    Testeado:Login, AlmacenarUsuario,AlmacenarTinket, EntradaUtilizada. TO DO: actualizar info ya que bb cambio

--- api_rest/views.py
+++ api_rest/views.py
@@ -30,10 +30,11 @@ def login(request):
 		user_name = str(received_data['correo'])
 		passw = str(received_data['pass'])
 		print(user_name, passw)
-		user = Usuario.objects.filter(username=user_name, password=passw)
-		if user is not None:
+		try: 
+			user = Usuario.objects.get(username=user_name, password=passw)
+			print(user)
 			return HttpResponse(json.dumps({'logged': True}), content_type = "application/json")
-		else:
+		except ObjectDoesNotExist:
 			return HttpResponse(json.dumps({'logged': False}), content_type = "application/json")
 
 #almacenar usuario
@@ -61,59 +62,6 @@ def almacenarUsuario(request):
 		return HttpResponse(json.dumps(response_data), content_type = "application/json")
 
 
-#buscar las entradas que posee cierto usuario para una determinada empresa 
-@login_required   #pistola
-def usarEntrada(request):
-	if request.method == 'POST':
-		received_data = json.loads(request.body.decode('utf-8'))
-		usuario_id = received_data['id_usuario']
-		empresa_id = received_data['id_empresa']
-		discount = False
-		try:
-			#repetir hasta encontrar una entrada valida
-			while not discount:
-				#obtener LA entrada mas cercana a vencer				
-				entrada = Tinket.objects.filter(usuario=usuario_id, empresa=empresa_id, valido=True).order_by('fecha_expiracion')[0]
-				if not entrada:
-					return_data = { 
-						"discount": False, 
-						#"id_tinket": entrada.id
-						"mensaje": "No posees entradas disponibles."
-					}
-					return HttpResponse(json.dumps(return_data), content_type = "application/json")
-
-				send_data = { 
-					"id_ticket": entrada.id_ticket 
-				}
-				empresa = Empresa.objects.get(pk=empresa_id)
-				url = empresa.ip+empresa.puerto+'/'+empresa.nombre+'/discount'
-
-				#Hacemos la consulta al servidor de la empresa
-				response = request.post(url, send_data)
-				response_data = json.loads(response.text('utf-8')) 
-
-				discount = response_data['discount']
-				Tinket.objects.filter(id=entrada.id).update(valido=False)
-
-				if discount == True:
-					return_data = { 
-						"discount": True, 
-						#"id_tinket": entrada.id
-						"mensaje": "Entrada utilizada exitosamente."
-					}
-				else:
-					return_data = { 
-						"discount": False, 
-						#"id_tinket": entrada.id
-						"mensaje": "No posees entradas disponibles."
-					}
-		except ObjectDoesNotExist:
-			return_data = { 
-					"discount": False, 
-					#"id_tinket": entrada.id					
-					"mensaje": "No posees entradas disponibles."
-				} 
-		return HttpResponse(json.dumps(return_data), content_type = "application/json")
 
 #@login_required        #app
 @csrf_exempt 
@@ -189,29 +137,46 @@ def detalleEntrada(request):
 	if request.method == 'POST':
 		received_data = json.loads(request.body.decode('utf-8'))
 		id_tinket = received_data['id_tinket']
-		empresa_nombre = received_data['empresa_nombre']
+		#empresa_nombre = received_data['empresa_nombre']
+
 		try:
-			entrada = Tinket.objects.get(pk=id_tinket)
-			
+			tinket_previo= Tinket.objects.get(id=id_tinket)
+			empresa_nombre= tinket_previo.empresa.nombre
+			if not tinket_previo:
+				response_data = { 
+					"mensaje": "Detalle entrada no disponible.",
+					"detalle": False
+					}
+
 			send_data = { 
-				"id_ticket": entrada.id_ticket 
+				"id_ticket": tinket_previo.id_ticket 
 			}
+
 			empresa = Empresa.objects.get(nombre=empresa_nombre)
-			url = empresa.ip+':'+empresa.puerto+'/'+empresa.nombre+'/casino/detalle'
+			url = 'http://'+empresa.ip+':'+empresa.puerto+'/'+empresa.nombre+'/detalle'
 
+			response = requests.post(url, json.dumps(send_data))
+			response_data = json.loads(response.text)		
 
-			response = request.post(url, send_data)
-			response_data = json.loads(response.text('utf-8')) #lo que me devuelve el oscar
+			#si existe, verifico si el estado continua siendo el mismo			
+			tinket_previo.valido=str(response_data['valido'])
+			tinket_previo.save()
 
-			tinket = Tinket(fecha_emision=response_data.fecha_emision,fecha_utilizacion=None,fecha_expiracion=response_data.fecha_expiracion,valido=response_data.valido,id_ticket=id_ticket,usuario=usuario,empresa=empresa.id)
-			tinket.save()
+			response_data = {
+				"id": tinket_previo.id,
+				"fecha_emision": tinket_previo.fecha_emision.isoformat(),
+				"fecha_utilizacion": tinket_previo.fecha_utilizacion.isoformat(),
+				"fecha_expiracion": tinket_previo.fecha_expiracion.isoformat(),
+				"valido": tinket_previo.valido,
+				"empresa": tinket_previo.empresa.nombre
+				}
 
+			
 		except ObjectDoesNotExist:
 			response_data = { 
 				"mensaje": "Detalle entrada no disponible",
 				"detalle": False
-#To DO: que el oscar nos devuelva detalle = true
-			}
+				}
 		return HttpResponse(json.dumps(response_data), content_type = "application/json") #envio a la app
 
 
@@ -229,33 +194,102 @@ def almacenarTinket(request):
 				"id_ticket": id_ticket 
 			}
 		try:
+			print(empresa_nombre)
+			aqui='uno'
 			empresa = Empresa.objects.get(nombre=empresa_nombre)
 			url = 'http://'+empresa.ip+':'+empresa.puerto+'/'+empresa.nombre+'/detalle'
 
 			response = requests.post(url, json.dumps(send_data))
 			response_data = json.loads(response.text)
 
+
 			tinket = Tinket(
 				fecha_emision=str(response_data['fecha_emision']),
-				fecha_utilizacion=None,
+				fecha_utilizacion='2050-12-12',
 				fecha_expiracion=str(response_data['fecha_expiracion']),
 				valido=str(response_data['valido']),
 				id_ticket=id_ticket,
 				usuario=usuario,
 				empresa=empresa)
-			tinket.save()
 
+			#si existe no  lo almacena
+			aqui='dos'
+			tinket_previo= Tinket.objects.get(id_ticket=id_ticket, empresa=empresa)
+			
 			response_data = { 
-				"mensaje": "Tinket almacenado exitosamente.",
-				"almacenar": True
-			}
-		except ObjectDoesNotExist:
-			response_data = { 
-				"mensaje": "La empresa no existe",
+				"mensaje": "Tinket ya existente.",
 				"almacenar": False
 			}
+		except ObjectDoesNotExist:
+			print(aqui)
+
+			if aqui == 'uno':
+				response_data = { 
+					"mensaje": "La empresa no existe",
+					"almacenar": False
+				}
+			if aqui == 'dos':
+				tinket.save()
+				response_data = { 
+					"mensaje": "Tinket almacenado exitosamente.",
+					"almacenar": True
+				}
 		return HttpResponse(json.dumps(response_data), content_type = "application/json") #envio a la app
 
+#buscar las entradas que posee cierto usuario para una determinada empresa 
+@csrf_exempt    #pistola
+def usarEntrada(request):   ##logica para el casino, usar la entrada mas cercana a vencer
+	if request.method == 'POST':
+		received_data = json.loads(request.body.decode('utf-8'))
+		usuario_id = received_data['id_usuario']
+		empresa_id = received_data['id_empresa']
+		discount = False
+		try:
+			#repetir hasta encontrar una entrada valida
+			while not discount:
+				#obtener LA entrada mas cercana a vencer				
+				entrada = Tinket.objects.filter(usuario=usuario_id, empresa=empresa_id, valido=True).order_by('fecha_expiracion')[0]
+				if not entrada:
+					return_data = { 
+						"discount": False, 
+						#"id_tinket": entrada.id
+						"mensaje": "No posees entradas disponibles."
+					}
+					return HttpResponse(json.dumps(return_data), content_type = "application/json")
+
+				send_data = { 
+					"id_ticket": entrada.id_ticket 
+				}
+
+				empresa = Empresa.objects.get(id=empresa_id)
+				url = 'http://'+empresa.ip+':'+empresa.puerto+'/'+empresa.nombre+'/discount'
+
+				#Hacemos la consulta al servidor de la empresa
+				response = requests.post(url, json.dumps(send_data))
+				response_data = json.loads(response.text)
+				discount = str(response_data['discount'])
+
+				
+
+				if discount == True:
+					entrada.valido= False
+					entrada.save()
+
+					return_data = { 
+						"discount": True, 
+						"mensaje": "Entrada utilizada exitosamente."
+					}
+				else:
+					return_data = { 
+						"discount": False, 
+						"mensaje": "No posees entradas disponibles."
+					}
+		except ObjectDoesNotExist:
+			return_data = { 
+					"discount": False, 				
+					"mensaje": "No posees entradas disponibles."
+				} 
+		return HttpResponse(json.dumps(return_data), content_type = "application/json")
 
 
 
